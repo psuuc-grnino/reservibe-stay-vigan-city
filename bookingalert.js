73d600fd07ba454a70f600721561a6cb0cd33f67
psuuc-grnino <132413896+psuuc-grnino@users.noreply.github.com>
2023-06-20 17:50:59 +0800

    Add files via upload

---
+++ bookingalert.js
@@ -0,0 +1,24 @@
+
+btnBook1.onclick = function(){
+    alert("Ready to view the booking details.")
+}
+
+btnBook2.onclick = function(){
+    alert("Ready to view the booking details.")
+}
+
+btnBook3.onclick = function(){
+    alert("Ready to view the booking details.")
+}
+
+btnBook4.onclick = function(){
+    alert("Ready to view the booking details.")
+}
+
+btnBook5.onclick = function(){
+    alert("Ready to view the booking details.")
+}
+
+btnBook6.onclick = function(){
+    alert("Ready to view the booking details.")
+}
